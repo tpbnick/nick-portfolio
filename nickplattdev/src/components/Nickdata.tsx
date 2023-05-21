ed4cb0c6b01df299ad9356964234d5cb5f1f1725
Nick <49327729+tpbnick@users.noreply.github.com>
2023-05-21 10:47:23 -0400

    Hover effect on logo

--- nickplattdev/src/components/Nickdata.tsx
+++ nickplattdev/src/components/Nickdata.tsx
@@ -4,7 +4,7 @@ const Nickdata = () => {
 	return (
 		<div className="w-90 mx-auto sm:w-full md:w-4/5 lg:w-3/4 xl:w-2/3 pt-3">
 			<a href={window.location.href}>
-				<img src={NickAtom} style={{ width: "10%", height: "auto" }} className="mx-auto NickInfo fade-in" alt="Atom" />
+				<img src={NickAtom} className="mx-auto fade-in transition-transform duration-300 transform-gpu hover:scale-110" style={{ width: "10%", height: "auto" }} alt="Logo" />
 			</a>
 			<p className="text-white text-2xl pt-5 pb-2">Nicholas Platt</p>
 			<p className="text-white text-l pb-8">
