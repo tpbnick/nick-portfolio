98b273448f84254fde97913f2ea18a4b5713f14b
Nick <49327729+tpbnick@users.noreply.github.com>
2023-05-20 23:09:38 -0400

    Netlfiy form possible fix

--- nickplattdev/src/components/Databox.tsx
+++ nickplattdev/src/components/Databox.tsx
@@ -189,6 +189,7 @@ const Databox = () => {
 	const contactData = (
 		<div className="flex justify-center text-white mx-auto max-w-4xl">
 			<form name="contact-nick" method="POST" data-netlify="true" data-netlify-recaptcha="true" className="mx-auto mb-5 w-full md:w-3/4 lg:w-1/2">
+				<input type="hidden" name="form-name" value="contact-nick" />
 				<div className="space-y-4">
 					<div className="flex justify-between items-center">
 						<label htmlFor="name" className="text-left pr-4">
