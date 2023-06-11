8b4c9246d156f73e7a722a876c67109310173083
tpbnick <nickplatt4@gmail.com>
2023-06-11 03:20:28 -0400

    Added triviadart to Projects.tsx

--- nickplattdev/src/components/Projects.tsx
+++ nickplattdev/src/components/Projects.tsx
@@ -2,6 +2,7 @@ import SoccerfrontImg from "../assets/soccerfront.webp";
 import UnbinImg from "../assets/unbin.webp";
 import DocsImg from "../assets/docs.webp";
 import PortfolioImg from "../assets/portfolio.webp";
+import TriviaDartImg from "../assets/triviadart.webp";
 
 const Projects = () => (
 	<div className="justify-center">
@@ -49,6 +50,30 @@ const Projects = () => (
 						<div className="badge badge-success">Docker</div>
 						<div className="badge badge-success">TailwindCSS</div>
 						<div className="badge badge-success">SQLite</div>
+					</div>
+				</div>
+			</div>
+			<div className="card w-full md:w-96 bg-base-100 shadow-xl m-2 mx-auto">
+				<figure>
+					<img src={TriviaDartImg} />
+				</figure>
+				<div className="card-body">
+					<p className="text-center text-2xl font-bold">
+						<a
+							href="https://triviadart.com"
+							target="_blank"
+							className="text-blue-300 hover:text-blue-500"
+						>
+							Trivia🎯
+						</a>
+					</p>
+					<p>Trivia Web App</p>
+					<div className="flex flex-wrap gap-2 justify-center">
+						<div className="badge badge-success">React</div>
+						<div className="badge badge-success">TypeScript</div>
+						<div className="badge badge-success">TailwindCSS</div>
+						<div className="badge badge-success">ExpressJS</div>
+						<div className="badge badge-success">MongoDB</div>
 					</div>
 				</div>
 			</div>
